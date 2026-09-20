85553d751751d55dadb22120f5b3e3add51fc8f5
gcoronar-prog <gcoronarc4621@gmail.com>
2026-09-19 22:50:41 -0300

    avances en manejo de errores en microservicio

--- clinica/src/main/java/com/clinica/clinica/controllers/clinicaController.java
+++ clinica/src/main/java/com/clinica/clinica/controllers/clinicaController.java
@@ -1,6 +1,5 @@
 package com.clinica.clinica.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
--- clinica/src/main/java/com/clinica/clinica/models/Clinica.java
+++ clinica/src/main/java/com/clinica/clinica/models/Clinica.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.*;
 
 
 @Entity
@@ -18,24 +19,53 @@ public class Clinica {
     @Column(name = "id_atencion")
     private Long idAtencion;
 
+    @Size(min = 1, max = 8, message = "El RUT debe tener entre 1 y 8 dígitos")
+    @NotBlank (message = "El RUT no puede estar vacío")
+    @Pattern (regexp = "\\d+", message = "El RUT debe contener solo dígitos")
     @Column(name = "rut_paciente")
     private Long rutPaciente;
-    
+
+    @Size (min = 1, max = 1, message = "El dígito verificador debe tener un solo carácter")
+    @NotBlank (message = "El dígito verificador no puede estar vacío")
+    @Pattern (regexp = "[0-9Kk]", message = "El dígito verificador debe ser un dígito o la letra K")
     @Column (name = "dv_paciente")
     private String dvPaciente;
 
+    @Size(min=4, max=50, message="El nombre del paciente debe tener entre 4 y 50 caracteres")
+    @Pattern (regexp = "^[a-zA-ZÀ-ÿ\\s]+$", message = "El nombre del paciente solo puede contener letras y espacios")
+    @NotBlank (message = "El nombre del paciente no puede estar vacío")
     @Column(name = "nombres_paciente")
     private String nombresPaciente;
+
+    @Size(min=4, max=50, message="El apellido del paciente debe tener entre 4 y 50 caracteres")
+    @Pattern (regexp = "^[a-zA-ZÀ-ÿ\\s]+$", message = "Los apellidos del paciente solo pueden contener letras y espacios")
+    @NotBlank (message = "Los apellidos del paciente no pueden estar vacíos")
     @Column(name = "apellidos_paciente")
     private String apellidosPaciente;
+
+    @NotBlank (message = "La fecha de nacimiento no puede estar vacía")
     @Column(name = "fecha_nacimiento")
     private LocalDate fechaNacimiento;
+
+
+    @NotBlank (message = "El historial médico no puede estar vacío")
     @Column(name = "historial_medico")
     private String historialMedico;
+
+    @NotBlank (message = "El número de atenciones no puede estar vacío")
+    @Pattern (regexp = "\\d+", message = "El número de atenciones debe contener solo dígitos")
     @Column(name = "numero_atenciones")
     private int NumeroAtenciones;
+
+    @Size(min=4, max=50, message="El nombre del médico debe tener entre 4 y 50 caracteres")
+    @Pattern (regexp = "^[a-zA-ZÀ-ÿ\\s]+$", message = "El nombre del médico solo puede contener letras y espacios")
+    @NotBlank (message = "El nombre del médico no puede estar vacío")
     @Column(name = "nombre_medico")
     private String NombreMedico;
+
+    @Size(min=4, max=50, message="El apellido del médico debe tener entre 4 y 50 caracteres")
+    @Pattern (regexp = "^[a-zA-ZÀ-ÿ\\s]+$", message = "El apellido del médico solo puede contener letras y espacios")
+    @NotBlank (message = "El apellido del médico no puede estar vacío")
     @Column(name = "apellido_medico")
     private String ApellidoMedico;
 
